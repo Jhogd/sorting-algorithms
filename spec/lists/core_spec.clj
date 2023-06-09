245f9eb468756e7f5ee9f340ef64b7cefb9b3928
jakeogden <jake.ogden@cleancoders.com>
2023-06-09 11:49:05 -0700

    update

--- spec/lists/core_spec.clj
+++ spec/lists/core_spec.clj
@@ -1,42 +1,120 @@
 (ns lists.core-spec
   (:require [speclj.core :refer :all]
             [lists.core :refer :all]))
+(def l (atom nil))
+(def n (atom nil))
+(def mylist (create-linked-list))
+(def mylist2 (create-linked-list))
+(def mylist3 (create-linked-list))
+(def mylist4 (create-linked-list))
+(def mylist5 (create-linked-list))
 
-(describe "creating linked-lists and array-lists from scratch"
-  (it "instantiates an array-list"
-    (should= {:kind :array :data []
-              :size 0} (create-array-list)))
 
-  (it "adds element to the array-list"
-    (should= {:kind :array, :data [1 2 3 4], :size 4} (Add {:kind :array, :data [1 2 3], :size 3} 4) ))
 
-  (it " gets a value from a array-list given an index"
-    (should= 3 (Get {:kind :array, :data [1 2 3 4], :size 4} 2))
-    (should= 4 (Get {:kind :array, :data [1 2 3 4], :size 4} 3)))
+(defn common-tests []
+  (list
 
-  (it "removes a value from an array-list given index"
-    (should= {:kind :array, :data [1 2 3], :size 3}
-            (Remove {:kind :array, :data [1 2 3 4] :size 4} 3)))
+  (it " gets a value from a list given an index"
+    (doseq [i (range 1 5)] (add @l i))
+    (should= 3 (get @l 2))
+    (should= 4 (get @l 3)))
+
+
+  (it "removes a value from a list given index"
+    (doseq [i (range 1 5)] (add @l i))
+    (remove @l 3)
+    (should= 3 (size @l))
+    (should= 1 (get @l 0))
+    (should= 2 (get @l 1))
+    (should= 3 (get @l 2))
+    )
 
   (it "returns the size of the array-list"
-    (should= 4 (Size {:kind :array, :data [1 2 3 4], :size 4})))
+    (doseq [i (range 1 5)] (add @l i))
+    (should= 4 (size @l)))
 
-  (it "creates a new linked list"
-    (should= {:kind :linked-list :data 5 :next nil} (create-node 5)))
+  #_(it "creates a new linked list"
+      (should= {:kind :linked-list, :head nil} @(create-linked-list)))
 
-  (it "adds to the end of the linked-list"
-    (should= {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}} (Add {:kind :linked-list :head nil} 10)))
 
-  (it "removes from a linked-list"
-    (should= {:kind :linked-list}  (Remove {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}} 0)))
+  #_(it "gets the size of the linked-list"
+      (should= 1 (size {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}})))
 
-  (it "gets a value from a given index"
-    (should= 10 (Get {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}} 0)))
+  #_(it "prints the linked-list"
+      (should= "10 ->" (with-out-str (display {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}}))))
 
-  (it "gets the size of the linked-list"
-    (should= 1 (Size {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}})))
+  ))
 
-  (it "prints the linked-list"
-    (should= "10 ->" (with-out-str (display-list {:kind :linked-list, :head {:kind :linked-list, :data 10, :next nil}}))))
+(describe "array-list"
+  (before (reset! l (create-array-list)))
 
+  (it "instantiates an array-list"
+    (should= 0 (size @l))
+    (should= [0 0 0 0 0 0 0 0 0 0] (vec @(:data @l))))
+
+  (it "adds element to the array-list"
+    (doseq [i (range 1 5)] (add @l i))
+    (should= [1 2 3 4 5 0 0 0 0 0] (vec @(:data (add @l 5))))
+
+  (common-tests)
   )
+(describe "linked-list"
+  (before (reset! n (create-linked-list)))
+  (it "instantiates a linked-list"
+    (should= {:kind :linked-list :head nil}
+             @(create-linked-list)))
+
+  (it "creates a new node"
+    (should= {:data 5 :next  nil} @(create-node 5)))
+
+
+  (it "retrieves the last node"
+    (should= {:data 5, :next nil}
+             @(last-node
+               (swap! mylist assoc-in [:head] (create-node 5))))
+    )
+
+  (it "adds to the linked-list"
+    (add-list mylist2 1)
+    (add-list mylist2 2)
+    (add-list mylist2 3)
+    (add-list mylist2 4)
+    (should= 1 (:data @(:head @mylist2)))
+    (should= 2 (:data @(:next @(:head @mylist2))))
+    )
+  (it "removes node given an index"
+    (add-list mylist3 4)
+    (add-list mylist3 2)
+    (add-list mylist3 1)
+    (add-list mylist3 6)
+    (remove-list mylist3 0)
+    (remove-list mylist3 1)
+    (should= 2 (get-list mylist3 0))
+    (should= 6 (get-list mylist3 1))
+    )
+
+  (it "gets data value from"
+    (add-list mylist4 3)
+    (add-list mylist4 2)
+    (add-list mylist4 6)
+    (add-list mylist4 8)
+    (should= 3 (get-list mylist4 0))
+    (should= 8 (get-list mylist4 3))
+    (should= 6 (get-list mylist4 2))
+    (should= 2 (get-list mylist4 1))
+    )
+
+  (it "gets the current size of the linked-list"
+    (add-list mylist5 3)
+    (add-list mylist5 2)
+    (add-list mylist5 6)
+    (add-list mylist5 8)
+    (should= 4 (size-list mylist5))
+    (should= 0 (size-list (create-linked-list)))
+    )
+
+  (it "displays the linked-node"
+    (should= "3 ->2 ->6 ->8 ->"  (with-out-str (display-list mylist5))))
+
+  (common-tests)
+  ))
--- src/lists/core.clj
+++ src/lists/core.clj
@@ -1,112 +1,144 @@
 (ns lists.core)
 
+
+(defmulti add :kind)
+(defmulti remove :kind)
+(defmulti get :kind)
+(defmulti size :kind)
+
 (defn create-array-list []
-  (let [data  []
-        size  0]
+  (let [data  (atom (make-array Integer/TYPE 10))
+        size  (atom 0)]
     {:kind :array :data data :size size}))
 
-(defmulti Add :kind)
-(defmulti Remove :kind)
-(defmulti Get :kind)
-(defmulti Size :kind)
 
+(defn ensure-capacity [arr-list]
+  (let [data (:data arr-list)
+        current-size (count @data)]
+    (when (= @(:size arr-list) current-size)
+      (swap! data #(into-array  (concat % (make-array Integer/TYPE current-size)))))))
 
-(defmethod Add :array [arr-list element]
-  (let [new-data (conj (:data arr-list) element)
-        new-size (inc (:size arr-list))]
-    (assoc arr-list :data new-data :size new-size)))
+(defn array-get [arr index]
+  (if (zero? index)
+    (first arr)
+    (recur (rest arr) (dec index))))
 
-(defmethod Get :array [arr-list index]
+(defmethod add :array [arr-list element]
   (let [data (:data arr-list)
         size (:size arr-list)]
-    (if (and (integer? index) (>= index 0) (< index size))
-      (loop [current data i index]
-        (if (empty? current)
-          nil
-          (if (zero? i)
-            (first current)
-            (recur (rest current) (dec i))))))))
+    (ensure-capacity arr-list)
+    (aset @data @size element)
+    (swap! (:size arr-list) inc)
+    arr-list))
 
-(defmethod Remove :array [arr-list index]
+(defmethod get :array [arr-list index]
   (let [data (:data arr-list)
         size (:size arr-list)]
-    (if (and (integer? index) (>= index 0) (< index size))
-      (let [new-data (vec (concat (subvec data 0 index) (subvec data (inc index))))
-            new-size (dec size)]
-        (assoc arr-list :data new-data :size new-size)))))
+    (if (and (integer? index) (>= index 0) (< index @size))
+      (array-get (vec @data) index)
+      (throw (Exception. "Index out of bounds")))))
 
-(defmethod Size :array [arr-list]
-  (:size arr-list))
+(defn array-set [arr index value]
+  (if (zero? index)
+    (conj (rest arr) value)
+    (assoc arr index value)))
+
+(defmethod remove :array [arr-list index]
+  (let [data (:data arr-list)
+        size (:size arr-list)]
+    (if (and (integer? index) (>= index 0) (< index @size))
+      (do
+        (dotimes [i (- @size (inc index))]
+          (array-set (vec @data) (+ index i) (array-get (vec @data) (+ (inc index) i))))
+        (swap! (:size arr-list) dec)
+        (array-set (vec @data) @size 0))
+      (throw (Exception. "Index out of bounds")))))
+
+(defmethod size :array [arr-list]
+  @(:size arr-list))
+
+
+(defn create-linked-list []
+  (atom {:kind :linked-list :head nil}))
 
 (defn create-node [data]
-  {:kind :linked-list :data data :next nil})
+  (atom {:data data :next nil}))
+
 
 (defn last-node [list]
   (loop [current (:head list)]
-    (if (nil? (:head current))
+    (if (nil? (:next @current))
       current
-      (recur (:next current)))))
+      (recur (:next @current)))))
 
-(defmethod Add :linked-list [list data]
+(defn add-list [list data]
   (let [new-node (create-node data)]
-    (if (nil? (:head list))
-      (assoc list :head new-node)
-      (let [last-node (last-node list)]
-        (assoc-in last-node [:next] new-node)
-        list))))
-
-(defn remove-node [node index]
-  (if (nil? node)
-    nil
-    (if (= index 1)
-      (:next node)
-      (assoc-in node [:next] (remove-node (:next node) (dec index))))))
-
-(defmethod Remove :linked-list [list index]
-  (if (nil? (:head list))
-    list
-    (if (= index 0)
-      (dissoc list :head)
-      (assoc list :head (remove-node (:head list) index))))
-  )
+    (if (nil? (:head @list))
+      (swap! list assoc-in [:head] new-node)
+      (let [last-node (last-node @list)]
+        (swap! last-node assoc-in [:next] new-node)))))
 
 
-(defmethod Get :linked-list [list index]
+
+
+(defn get-list [list index]
     (loop [i 0
-           current (:head list)]
-      (if (or (nil? current) (= i index))
-        (:data current)
-        (recur (inc i) (:next current)))))
+           current (:head @list)]
+      (if (= i index)
+        (:data @current)
+        (recur (inc i) (:next @current)))))
 
-(defmethod Size :linked-list [list]
-    (loop [count 0 node (:head list)]
+(defn size-list [list]
+    (loop [count 0 node (:head @list)]
       (if (nil? node)
         count
-        (recur (inc count) (:next node)))))
+        (recur (inc count) (:next @node)))))
+
+
+(defn remove-list [list index]
+  (if (nil? (:head @list))
+    list
+    (let [size (size-list list)]
+      (if (or (>= index size) (<= index -1))
+        list
+        (let [values (vec (for [i (range size)
+                                :when (not= i index)]
+                            (get-list list i)))]
+          (swap! list @(create-linked-list))
+          (doseq [value values]
+            (add-list list value))
+          )))))
 
 (defn display-list [list]
-  (if (nil? (:head list))
+  (loop [current (:head @list)]
+  (if (nil? current)
     nil
     (do
-      (print (:data (:head list)) "->")
-      (display-list (assoc list :head (:next (:head list)))))))
-
-(def my-list {:kind :linked-list :head nil})
-(def my-list (Add my-list 12))
+      (print (:data @current) "->")
+      (recur (:next @current))))))
 
 (def my-array (create-array-list))
-(def my-array (Add my-array 5))
+(def my-list (create-linked-list))
+
+
+(doseq [i (range 1 10)] (add my-array i))
+(doseq [i (range 1 10)] (add-list my-list i))
 
 (defn -main [& args]
+  (println "\n Array-list----------")
+  (println (vec @(:data my-array)))
+  (println (get my-array 8))
+  (println (remove my-array 8))
+  (println "\n linked-node---------")
+  (println (get-list my-list 0))
+  (println (get-list my-list 5))
+  (println (get-list my-list 7))
+  (remove-list my-list 1)
   (display-list my-list)
-  (println "\n----------")
-  (println (:data my-array))
-  (println (Get my-array 0))
-  (println (Get my-list 0))
-  (println (:data (Remove my-array 0)))
-  (display-list (Remove my-list 0))
-  (time (Get my-array 0))
-  (time (Get my-list 0))
+  (println my-list)
+
+
+
   )
 
 
